e3ba83e299731f7cd86ef8821b8c00ae7a5c7c78
Friedrich W. H. Kossebau <kossebau@kde.org>
2010-08-01 12:09:58 +0000

    added: pkg-config file
    svn path=/branches/cagibi/0.1/; revision=1157878

---
+++ daemon/cagibi.pc.cmake
@@ -0,0 +1,7 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${CMAKE_INSTALL_PREFIX}/bin
+
+Name: Cagibi daemon
+Description: experimental cache/proxy system for the SSDP (Simple Service Discovery Protocol) part of UPnP.
+Version: ${CAGIBI_VERSION}
+Requires: QtCore QtXml QtNetwork QtDBus
