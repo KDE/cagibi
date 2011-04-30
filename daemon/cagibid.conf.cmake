bf6ea4a858b04e34c190fbb77dbc5691169df4a3
Friedrich W. H. Kossebau <kossebau@kde.org>
2011-04-30 02:04:04 +0200

    Added: install a basic config file, with comments and example settings

---
+++ daemon/cagibid.conf.cmake
@@ -0,0 +1,6 @@
+[General]
+# Time (in sec) without any discovered services and any client query
+# activity after which a shutdown of the daemon is triggered (0 = no shutdown).
+# Default time is @CAGIBI_DAEMON_SHUTDOWN_SECS@.
+# Uncomment the following line to change this time, in the example to 23 secs:
+# ShutDownTimeout = 23
