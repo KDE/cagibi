f322130640b22ea9795270780af575eb82d9a8ef
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2013-05-02 01:11:39 +0300

    C++11 support: separate literals and identifiers with space.

--- daemon/ssdpwatcher.cpp
+++ daemon/ssdpwatcher.cpp
@@ -119,8 +119,8 @@ qDebug() << "Trying to find UPnP devices on the local network";
     // send a HTTP M-SEARCH message to 239.255.255.250:1900
     const QByteArray searchMessage =
         "M-SEARCH * HTTP/1.1\r\n"
-        "HOST: "SSDP_BROADCAST_ADDRESS":"SSDP_PORT"\r\n"
-        "ST:"ROOTDEVICE"\r\n"
+        "HOST: " SSDP_BROADCAST_ADDRESS ":" SSDP_PORT "\r\n"
+        "ST:" ROOTDEVICE "\r\n"
 //         "ST: ssdp:all\r\n"
         "MAN:\"ssdp:discover\"\r\n"
         "MX:" + QByteArray::number(searchTimeout) + "\r\n" // max number of seconds to wait for response
